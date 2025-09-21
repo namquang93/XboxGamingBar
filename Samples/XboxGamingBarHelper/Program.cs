4ee071823257e2c768fc5f0aba8d0d6030c45d29
Nam Nguyen <ngqnam@gmail.com>
2025-09-21 07:23:20 +0700

    Remove AnyCPU, ARM, ARM64 targets

--- Samples/XboxGamingBarHelper/Program.cs
+++ Samples/XboxGamingBarHelper/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RTSSSharedMemoryNET;
 
 namespace XboxGamingBarHelper
 {
@@ -10,6 +12,9 @@ namespace XboxGamingBarHelper
     {
         static void Main(string[] args)
         {
+            var osdEntries = OSD.GetOSDEntries();
+            // osdEntries[0].Text = "Hello from XboxGamingBarHelper!";
+            Debug.WriteLine($"OSD {osdEntries.Length} APP {OSD.GetAppEntries().Length}");
         }
     }
 }
