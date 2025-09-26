9b2f0a45e7dece0e24c82311ed10228d5b9a5a6c
Nam Nguyen <ngqnam@gmail.com>
2025-09-27 06:32:59 +0700

    Find sensors and save to fields

--- Samples/XboxGamingBarHelper/Performance/PerformanceManager.cs
+++ Samples/XboxGamingBarHelper/Performance/PerformanceManager.cs
@@ -1,22 +1,83 @@
-﻿using LibreHardwareMonitor;
-using LibreHardwareMonitor.Hardware;
+﻿using LibreHardwareMonitor.Hardware;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Xml.Linq;
 
 namespace XboxGamingBarHelper.Performance
 {
+    [AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+    public sealed class HardwareSensorAttribute : Attribute
+    {
+        public string Name { get; }
+        public HardwareType HardwareType { get; }
+        public SensorType SensorType { get; }
+
+        public HardwareSensorAttribute(string name, HardwareType hardwareType, SensorType sensorType)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            HardwareType = hardwareType;
+            SensorType = sensorType;
+        }
+    }
+
     internal static class PerformanceManager
     {
         private static Computer computer;
+        private static IVisitor updateVisitor;
 
-        private static float battery = 0.0f;
-        public static float Battery { get { return battery; } }
+        #region CPU
+        [HardwareSensor("CPU Total", HardwareType.Cpu, SensorType.Load)]
+        private static ISensor cpuUsage;
+        public static ISensor CPUUsage => cpuUsage;
+
+        [HardwareSensor("Core #1", HardwareType.Cpu, SensorType.Clock)]
+        private static ISensor cpuClock;
+        public static ISensor CPUClock => cpuClock;
+
+        [HardwareSensor("Package", HardwareType.Cpu, SensorType.Power)]
+        private static ISensor cpuWattage;
+        public static ISensor CPUWattage => cpuWattage;
+        #endregion
+
+        #region GPU
+        [HardwareSensor("GPU Core", HardwareType.GpuAmd, SensorType.Load)]
+        private static ISensor gpuUsage;
+        public static ISensor GPUUsage => gpuUsage;
+
+        [HardwareSensor("GPU Core", HardwareType.GpuAmd, SensorType.Clock)]
+        private static ISensor gpuClock;
+        public static ISensor GPUClock => gpuClock;
+
+        [HardwareSensor("GPU Core", HardwareType.GpuAmd, SensorType.Power)]
+        private static ISensor gpuWattage;
+        public static ISensor GPUWattage => gpuWattage;
+        #endregion
+
+        #region Memory
+        [HardwareSensor("Memory", HardwareType.Memory, SensorType.Load)]
+        private static ISensor memoryUsage;
+        public static ISensor MemoryUsage => memoryUsage;
+
+        [HardwareSensor("Memory Used", HardwareType.Memory, SensorType.Data)]
+        private static ISensor memoryUsed;
+        public static ISensor MemoryUsed => memoryUsed;
+        #endregion
+
+        #region Battery
+        [HardwareSensor("Charge Level", HardwareType.Battery, SensorType.Level)]
+        private static ISensor batteryPercent;
+        public static ISensor BatteryPercent => batteryPercent;
+
+        [HardwareSensor("Remaining Time (Estimated)", HardwareType.Battery, SensorType.TimeSpan)]
+        private static ISensor batteryRemainTime;
+        public static ISensor BatteryRemainTime => batteryRemainTime;
+        #endregion
 
         internal static void Initialize()
         {
+            // Initialize the computer sensors
             computer = new Computer
             {
                 IsCpuEnabled = true,
@@ -28,8 +89,35 @@ namespace XboxGamingBarHelper.Performance
                 IsStorageEnabled = true,
                 IsBatteryEnabled = true,
             };
+            updateVisitor = new UpdateVisitor();
 
             computer.Open();
+            computer.Accept(updateVisitor);
+
+            // Initialize sensor fields
+
+            Dictionary<(string Name, HardwareType HW, SensorType ST), FieldInfo> hardwareSensorFields = new Dictionary<(string Name, HardwareType HW, SensorType ST), FieldInfo>();
+            foreach (var field in typeof(PerformanceManager).GetFields(BindingFlags.Static | BindingFlags.NonPublic))
+            {
+                var hardwareSensorAttribute = field.GetCustomAttributes(typeof(HardwareSensorAttribute), inherit: true)
+                                .Cast<HardwareSensorAttribute>()
+                                .FirstOrDefault();
+                if (hardwareSensorAttribute == null) continue;
+
+                hardwareSensorFields.Add((hardwareSensorAttribute.Name, hardwareSensorAttribute.HardwareType, hardwareSensorAttribute.SensorType), field);
+            }
+
+            foreach (IHardware hardware in computer.Hardware)
+            {
+                foreach (ISensor sensor in hardware.Sensors)
+                {
+                    if (hardwareSensorFields.TryGetValue((sensor.Name, hardware.HardwareType, sensor.SensorType), out FieldInfo fieldInfo))
+                    {
+                        fieldInfo.SetValue(null, sensor);
+                        Console.WriteLine("Found hardware Sensor: {0}, value: {1}, type: {2}", sensor.Name, sensor.Value, sensor.SensorType.ToString());
+                    }
+                }
+            }
         }
 
         internal static void Update()
@@ -37,28 +125,7 @@ namespace XboxGamingBarHelper.Performance
             if (computer == null)
                 return;
 
-            var updateVisitor = new UpdateVisitor();
             computer.Accept(updateVisitor);
-
-            foreach (IHardware hardware in computer.Hardware)
-            {
-                Console.WriteLine("Hardware: {0}", hardware.Name);
-
-                foreach (IHardware subhardware in hardware.SubHardware)
-                {
-                    Console.WriteLine("\tSubhardware: {0}", subhardware.Name);
-
-                    foreach (ISensor sensor in subhardware.Sensors)
-                    {
-                        Console.WriteLine("\t\tSensor: {0}, value: {1}", sensor.Name, sensor.Value);
-                    }
-                }
-
-                foreach (ISensor sensor in hardware.Sensors)
-                {
-                    Console.WriteLine("\tSensor: {0}, value: {1}", sensor.Name, sensor.Value);
-                }
-            }
         }
     }
 }
