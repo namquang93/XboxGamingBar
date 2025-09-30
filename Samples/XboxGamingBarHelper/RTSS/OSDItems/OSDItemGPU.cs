98f8c7085c4a885b246ea6547138e3319a6449ce
Nam Nguyen <ngqnam@gmail.com>
2025-10-01 06:28:01 +0700

    Adjust GPU OSD

--- Samples/XboxGamingBarHelper/RTSS/OSDItems/OSDItemGPU.cs
+++ Samples/XboxGamingBarHelper/RTSS/OSDItems/OSDItemGPU.cs
@@ -27,14 +27,14 @@ namespace XboxGamingBarHelper.RTSS.OSDItems
             if (osdLevel >= 3)
             {
                 osdItems.Add(new OSDItemValue(gpuUsageSensor != null ? gpuUsageSensor.Value.Value : -1.0f, "%"));
-                osdItems.Add(new OSDItemValue(gpuTemperatureSensor != null ? gpuTemperatureSensor.Value.Value : -1.0f, "°C"));
+                osdItems.Add(new OSDItemValue(gpuWattageSensor != null ? gpuWattageSensor.Value.Value : -1.0f, "W"));
             }
 
             // for level 4, show GPU usage, clock speed, wattage and temperature.
             if (osdLevel >= 4)
             {
                 osdItems.Add(new OSDItemValue(gpuClockSensor != null ? gpuClockSensor.Value.Value : -1.0f, "MHz"));
-                osdItems.Add(new OSDItemValue(gpuWattageSensor != null ? gpuWattageSensor.Value.Value : -1.0f, "W"));
+                osdItems.Add(new OSDItemValue(gpuTemperatureSensor != null ? gpuTemperatureSensor.Value.Value : -1.0f, "°C"));
             }
 
             return osdItems;
