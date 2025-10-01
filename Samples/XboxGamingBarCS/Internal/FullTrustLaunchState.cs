45a287411682006b9b6735e96ee9e37f2825419a
Nam Nguyen <ngqnam@gmail.com>
2025-10-01 20:58:22 +0700

    Update widget

---
+++ Samples/XboxGamingBarCS/Internal/FullTrustLaunchState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XboxGamingBarCS.Internal
+{
+    public enum FullTrustLaunchState
+    {
+        NotLaunched = 0,
+        Reconnecting = 1,
+        Launching = 2,
+        Launched = 3,
+    }
+}
