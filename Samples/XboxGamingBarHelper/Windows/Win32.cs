593eba2c36545aeb0e0c37fb552f723a2de20c55
Nam Nguyen Quang <ngqnam@gmail.com>
2025-10-02 07:02:40 +0700

    Adjust log and add some Win32 functions (#5)
    * Add function to find the active window
    
    * Adjust nlog config
    
    * Print some info to log

---
+++ Samples/XboxGamingBarHelper/Windows/Win32.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace XboxGamingBarHelper.Windows
+{
+    internal class Win32
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out IntPtr lpdwProcessId);
+    }
+}
