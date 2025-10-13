2b539b1cb5cd6bb1ed4ff5dc139ecf3e184a3eb6
Nam Nguyen <ngqnam@gmail.com>
2025-10-14 06:30:14 +0700

    Format watt string

---
+++ Samples/XboxGamingBar/WattStringConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace XboxGamingBar
+{
+    public class WattStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return $"{value}W";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
