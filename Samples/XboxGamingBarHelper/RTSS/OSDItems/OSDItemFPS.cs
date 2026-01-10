fb1c0becbd478902220f077bb4ccff220ca5d3b8
Nam Nguyen <ngqnam@gmail.com>
2026-01-10 12:16:20 +0700

    Add frametime graph to OSD

--- Samples/XboxGamingBarHelper/RTSS/OSDItems/OSDItemFPS.cs
+++ Samples/XboxGamingBarHelper/RTSS/OSDItems/OSDItemFPS.cs
@@ -2,9 +2,19 @@
 {
     internal class OSDItemFPS : OSDItem
     {
+        private const string FPS_HYPERTEXT = "<C=FF0000><APP><C> <C=FFFFFF><FR><S=50> FPS<S><C>";
+        private const string FPS_WITH_GRAPH_HYPERTEXT = "<C=FF0000><APP><C> <C=FFFFFF><FR><S=50> FPS<S><C> <C=00FF00><G=<FT>,-20,-1><C>";
+
         public override string GetOSDString(int osdLevel)
         {
-            return "<C=FF0000><APP><C> <C=FFFFFF><FR><S=50> FPS<S><C>";
+            if (osdLevel <= 1)
+            {
+                return FPS_HYPERTEXT;
+            }
+            else
+            {
+                return FPS_WITH_GRAPH_HYPERTEXT;
+            }
         }
     }
 }
